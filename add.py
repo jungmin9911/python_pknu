efb38b0cd47747413399373aaa70103fc46b3dc7
jungmin9911 <jmahn1819@gmail.com>
2025-02-11 16:23:23 +0900

    프로그램의 입출력

---
+++ add.py
@@ -0,0 +1,4 @@
+import sys
+value = sys.argv[1:]  # 인자를 받음
+result = int(value[0]) + int(value[1]) + int(value[2])
+print(result)
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+args = sys.argv  # 인자를 받음
+for i in args:
+    print(i)
